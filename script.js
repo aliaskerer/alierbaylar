cc9595d7ac74e7732309ca70732322c214f03023
aliaskerer <154758581+aliaskerer@users.noreply.github.com>
2024-05-16 18:55:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const registerButton = document.getElementById("register");
+
+    registerButton.addEventListener("click", function() {
+        alert("Kayıt Başarılı!");
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const registerButton = document.getElementById("register");
+
+    registerButton.addEventListener("click", function() {
+        alert("Kayıt Başarılı!");
+    });
+});
